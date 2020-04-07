5f84d4cf0c028f41e3912e61d08ff4534dd29995
tatiana110504 <60788560+tatiana110504@users.noreply.github.com>
2020-04-06 22:20:22 -0400

    commit 3

--- bot.py
+++ bot.py
@@ -18,41 +18,35 @@ async def say_hello(ctx):
     await ctx.send(f"Hi {ctx.author.display_name}")
     
 @bot.command()
-async def reservations(ctx):
-    await ctx.send(while answer != ('N','NO','n','no'):
+async def reservations():
 print("RESREVE")
 answer = ("Y")
 
 while answer != ('N','NO','n','no'):
-    print("1.Check pnr status")
-    print("2.Table and Game Reservation")
-    option = int(input("\nEnter your option : "))
+    print("1.Table and Game Reservation")
+    option = int(input("Enter your option: "))
     
-    if option == 1:
-        print("Your pnr status is t2")
-        exit(0)
-  
-    elif option == 2:
-        people = int(input("\nEnter no. of people at table: "))
+   if option == 1:
+        people = int(input("Enter # of people at table: "))
         name_l = []
         game_l = []
         table_l = []
         for p in range(people):2
-        name = str(input("\nName : "))
+        name = str(input("Name: "))
         name_l.append(name)
-        game = str(input("\nGame  : "))
+        game = str(input("Game: "))
         game_l.append(game)
-        table  = str(input("\nTable number : "))
+        table  = str(input("Table number: "))
         table_l.append(table)
 
-        answer = str(input("\nOops! Forget someone : "))
+        answer = str(input("Did you forget anyone?: "))
         if answer in ('y','YES','yes','Yes'):
                 answer = ('Y')
         else:
                     x=0
-        print("People : ",people)
-        print("Name : ", name_l[x])
-        print("Game  : ", game_l[x])
-        print("Table : ", table_l[x])
+        print("People: ",people)
+        print("Name: ", name_l[x])
+        print("Game: ", game_l[x])
+        print("Table: ", table_l[x])
         x += 1
 bot.run(TOKEN)
